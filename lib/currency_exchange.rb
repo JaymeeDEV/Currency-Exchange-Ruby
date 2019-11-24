b82fc6f8d2b5c2c21e598f970bdd5825b4058807
JaymeeDEV <jamiekellybrown@gmail.com>
2019-11-24 20:58:54 +0000

    Add support for base currency

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -4,9 +4,13 @@ module CurrencyExchange
 
   @currency_file = File.read('./data/eurofxref-hist-90d.json')
   @data = JSON.parse(@currency_file)
+  @base_currency = 'EUR'
 
   def self.rate(date, from_currency, to_currency)
     date = date.strftime('%Y-%m-%d')
-    @data[date][to_currency] / @data[date][from_currency]
+    to_currency = (to_currency == @base_currency) ? 1 : @data[date][to_currency]
+    from_currency = (from_currency == @base_currency) ? 1 : @data[date][from_currency]
+    to_currency / from_currency
   end
+
 end
