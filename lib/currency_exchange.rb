69e4bfeb0f216338a5667e1acc388e8bbe9ccef5
JaymeeDEV <jamiekellybrown@gmail.com>
2019-11-24 22:42:13 +0000

    Testing

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -10,13 +10,13 @@ module CurrencyExchange
 
   def self.rate(date, from_currency, to_currency)
     date = date.strftime('%Y-%m-%d')
-    # Raise Errors
-    raise 'There is no rate for the date provided' unless @data[date]
-    raise 'Unable to calculate exchange rate' unless @data[date][to_currency] && @data[date][from_currency]
     # Calculations
+    raise 'There is no rate for the date provided' unless @data[date]
     to_currency = (to_currency == @base_currency) ? 1 : @data[date][to_currency]
     from_currency = (from_currency == @base_currency) ? 1 : @data[date][from_currency]
+    raise 'Unable to calculate exchange rate' unless to_currency && from_currency
     to_currency / from_currency
+    
   end
 
 end
--- test/currency_exchange_test.rb
+++ test/currency_exchange_test.rb
@@ -1,23 +1,50 @@
-# These are just suggested definitions to get you started.  Please feel
-# free to make any changes at all as you see fit.
-
-
-# http://test-unit.github.io/
 require 'test/unit'
 require 'currency_exchange'
 require 'date'
 
 class CurrencyExchangeTest < Test::Unit::TestCase
-  def setup
-  end
+  def setup; end
 
   def test_non_base_currency_exchange_is_correct
     correct_rate = 1.2870493690602498
-    assert_equal correct_rate, CurrencyExchange.rate(Date.new(2018,11,22), "GBP", "USD")
+    assert_equal correct_rate, 
+                 CurrencyExchange.rate(Date.new(2018,11,22), "GBP", "USD")
   end
 
-  def test_base_currency_exchange_is_correct
-    correct_rate = 0.007763975155279502
-    assert_equal correct_rate, CurrencyExchange.rate(Date.new(2018,11,22), "JPY", "EUR")
+  def test_non_base_currency_exchange_is_correct_2
+    correct_rate = 2.0590988747463568 
+    assert_equal correct_rate,
+                 CurrencyExchange.rate(Date.new(2018,11,22), "BRL", "HKD")
   end
+
+  def test_base_to_currency_exchange_is_correct
+    correct_rate = 0.007763975155279502
+    assert_equal correct_rate, 
+                 CurrencyExchange.rate(Date.new(2018,11,22), "JPY", "EUR")
+  end
+
+  def test_from_base_currency_exchange_is_correct
+    correct_rate = 141.2
+    assert_equal correct_rate,
+                 CurrencyExchange.rate(Date.new(2018,11,22), "EUR", "ISK")
+  end
+
+  def test_false_date_raises_error
+    assert_raise do
+      CurrencyExchange.rate(Date.new(2017,11,22), "EUR", "ISK")
+    end
+  end
+
+  def test_failing_from_currency_raises_error
+    assert_raise do
+      CurrencyExchange.rate(Date.new(2018,11,22), "FAIL", "ISK")
+    end
+  end
+
+  def test_failing_to_currency_raises_error
+    assert_raise do
+      CurrencyExchange.rate(Date.new(2018,11,22), "EUR", "FAIL")
+    end
+  end
+  
 end
